52b370318fa6458600d7a52d755c06d183067bf3
Andrey Popkov <popkov.info@gmail.com>
2023-01-23 17:19:49 +0300

    Add Map and Route section

--- src/js/_components.js
+++ src/js/_components.js
@@ -5,4 +5,5 @@ import './components/rating';
 import './components/forms';
 import './components/video';
 import './components/show-more';
+import './components/map';
 // import './components/to-top'; // экранировать код, выдает ошибку
